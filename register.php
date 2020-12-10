4e086f9e08849087a11da722f092f99c39a63e78
Asif Mohammad <asifratul45@gmail.com>
2020-12-11 01:02:00 +0600

    regex added

--- register.php
+++ register.php
@@ -1,3 +1,66 @@
+<?php
+    function filterInput($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $name = $email = $password = $password2 = $address = $city = $country = $countryCode = $gender = "";
+    $nameErr = $emailErr = $passwordErr = $addressErr = $cityErr = $countryErr = $countryCodeErr = $ageErr = "";
+    $spaceRegex = "/\s\s+/";
+    $nameRegex = "/^[a-zA-Z'\. ]*$/";
+    $emailRegex = "/^[a-z\d\._-]+@([a-z\d-]+\.)+[a-z]{2,6}$/i";
+    $countryCodeRegex = "/^\d{1-3}(-\d{3-4})?$/";
+
+    if(isset($_POST["submit"])) {
+        // Name
+        $name = filterInput($_POST["name"]);
+        $name = ucwords($name);
+        $name = preg_replace($spaceRegex, ' ', $name);
+
+        if(!preg_match($nameRegex, $name)) {
+            $nameErr = "* Invalid name";
+        }
+
+        // Email
+        $email = filterInput($_POST['email']);
+        if(!preg_match($emailRegex, $email)) {
+            $emailErr = "* Invalid email";
+        }
+
+        // Password
+        $password = $_POST['password1'];
+        $password2 = $_POST['password2']; 
+        if($password !== $password2) $passwordErr = "* Passwords don't match";
+
+        // Address
+        $address = filterInput($_POST['address']);
+        $address = preg_replace($spaceRegex, ' ', $address);
+        if(strlen($address) < 10) $addressErr = "* Incomplete address";
+
+        // City
+        $city = filterInput($_POST['city']);
+        if(!strlen($city)) $cityErr = "* Invalid city";
+
+        // Country
+        $country = filterInput($_POST['country']);
+        if(!strlen($country)) $countryErr = "* Invalid country";
+
+        // Country Code
+        $countryCode = $_POST["countryCode"];
+        if(!preg_match($countryCodeRegex, $countryCode)) $countryCodeErr = "* Invalid country code";
+
+        // Date of Birth
+        $dob = $_POST['dob'];
+        $todayDate = date("Y-m-d");
+        $birthYear = explode('-', $dob)[0];
+        $thisYear = explode('-', $todayDate)[0];
+        $age = $thisYear - $birthYear;
+        if($age < 18) $ageErr ="* Too young to register";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,67 +78,75 @@
     <div class="container">
         <div class="log-in">
             <div class="text-center">
-                <img src="images/auction.png" alt=""> 
-                <h1>Registration Form</h1>
+                <img src="images/auction.png" alt="">
             </div>
             <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <label for="fname">First Name <span class="red">*</span></label>
-                        <input type="text" class="form-control" id="fname" placeholder="First Name" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="lname">Last Name <span class="red">*</span></label>
-                        <input type="text" class="form-control" id="lname" placeholder="Last Name" required>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="phone">Phone <span class="red">*</span></label>
-                        <input type="text" class="form-control" id="phone" placeholder="Phone" required>
+                        <label for="name">Full Name <span class="red">*</span></label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Muhammad Abu Bakar Siddique" value="<?php echo $name; ?>" minlength="5" maxlength="35" required>
+                        <span class="pink"><?php echo $nameErr; ?></span>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="email">Email <span class="red">*</span></label>
-                        <input type="email" class="form-control" id="email" placeholder="Email" required>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?php echo $email; ?>" maxlength="35" required>
+                        <span class="pink"><?php echo $emailErr; ?></span>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="address">Address <span class="red">*</span></label>
-                    <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="password1">Password <span class="red">*</span></label>
-                        <input type="password" class="form-control" id="password1" placeholder="Password" required>
+                        <input type="password" class="form-control" id="password1" name="password1" minlength="6" maxlength="20" placeholder="Password" value="<?php echo $password; ?>" required>
+                        <span class="pink"><?php echo $passwordErr; ?></span>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="password2">Confirm Password <span class="red">*</span></label>
-                        <input type="password" class="form-control" id="password2" placeholder="Confirm Password" required>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-5">
-                        <label for="city">City <span class="red">*</span></label>
-                        <input type="text" class="form-control" id="city" placeholder="City" required>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="country">Country <span class="red">*</span></label>
-                        <input type="text" class="form-control" id="country" placeholder="Country" required>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="zip">Zip <span class="red">*</span></label>
-                        <input type="text" class="form-control" id="zip" placeholder="Zip" required>
+                        <input type="password" class="form-control" id="password2" name="password2" minlength="6" maxlength="20" placeholder="Confirm Password" value="<?php echo $password2; ?>" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="allow" required>
-                        <label class="form-check-label" for="allow">
-                            I aggree to allow cookies to this website
-                        </label>
+                    <label for="address">Address <span class="red">*</span></label>
+                    <input type="text" class="form-control" id="address" name="address" placeholder="1234 Main St" value="<?php echo $address; ?>" required>
+                    <span class="pink"><?php echo $addressErr; ?></span>
+                    </div>
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="city">City <span class="red">*</span></label>
+                        <input type="text" class="form-control" id="city" name="city" maxlength="20" placeholder="City" value="<?php echo $city; ?>" required>
+                        <span class="pink"><?php echo $cityErr; ?></span>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="country">Country <span class="red">*</span></label>
+                        <input type="text" class="form-control" id="country" name="country" maxlength="20" placeholder="Country" value="<?php echo $country; ?>" required>
+                        <span class="pink"><?php echo $countryErr; ?></span>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="countryCode">Country Code <span class="red">*</span></label>
+                        <input type="text" class="form-control" id="countryCode" name="countryCode" maxlength="7" placeholder="Country Code" value="<?php echo $countryCode; ?>" required>
+                        <span class="pink"><?php echo $countryCodeErr; ?></span>
                     </div>
                 </div>
-                <button type="submit" name="submit" class="btn btn-primary">Sign Up</button>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="gender">Gender</label>
+                        <select id="gender" name="gender" required>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="dob">Date of Birth <span class="red">*</span></label>
+                        <input type="date" class="form-control" id="dob" name="dob" value="<?php echo $dob; ?>" required>
+                        <span class="pink"><?php echo $ageErr; ?></span>
+                    </div>
+                </div>
+                <div class="form-check h5">
+                    <input class="form-check-input" type="checkbox" id="allow" required>
+                    <label class="form-check-label" for="allow">
+                        I agree to allow cookies to this website
+                    </label>
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary mt-3">Sign Up</button>
             </form>
 
             <p class="login-instead text-center">Already have an account? <a href="index.php">Login Instead</a></p>
